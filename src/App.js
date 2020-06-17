53a23067e99c481807941e269364202e43a9049f
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-17 23:02:31 +0100

    routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import "./App.css";
+import Nav from "./components/Nav";
+import OnTheMenu from "./components/OnTheMenu";
+import Recipes from "./components/Recipes";
 
 function App() {
-	return <div className="App">
-		
-	</div>;
+	
+	return 	<Router>
+				<div className="App">
+					<Nav/>
+					<Route path="/" exact component={OnTheMenu} />
+					<Route path="/recipes" exact component={Recipes} />
+				</div>
+			</Router>
 }
 
 export default App;
