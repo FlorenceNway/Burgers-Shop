7e543f3cec713854d80c044e10ac2e9e387b02e1
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-17 23:45:32 +0100

    render recipes

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
+import API from './API'
+import burger from '../images/burgers/burger1.png'
 
 const Recipes = () => {
+
+    const [recipes, setRecipes] = useState([])
+
+    useEffect(() => {
+        API.getRecipes().then(recipes => //console.log(recipes)
+             setRecipes(recipes)
+        )
+    },[])
+ console.log(recipes)
     return <div>
         <h1>Recipes</h1>
+        <button>Get Started</button>
+        {recipes.map(recipe => (
+            <ul>
+                <li>{recipe.title}</li>
+                <li>{recipe.description}</li>
+                <li>
+                    <img src={`/src/images/burgers/burger1.png`}></img>
+                </li>
+            </ul>
+        ))}
     </div>
 }
 
