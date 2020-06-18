c77b0c54d483e547fb926b6fbfe2e028bbc6bce6
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-18 23:58:15 +0100

    add to basket

--- src/components/RecipeDetail.js
+++ src/components/RecipeDetail.js
@@ -2,17 +2,43 @@ import React, { useState,useEffect } from 'react';
 import API from './API'
 import "./style/Recipes.scss"
 
-const RecipeDetail = ({match}) => {
+const RecipeDetail = ({match, setQtyCounter, qtyCounter}) => {
     
     const [recipe, setRecipe] = useState({})
+    const [orders, setOrders] = useState([])
+    const [order, setOrder] = useState({
+            "quantity": 0,
+            "product_id": null
+    })
+    
+    const id = match.params.id
 
     useEffect(() => {
-        API.getRecipe(match.params.id).then(recipe => setRecipe(recipe))
-        console.log(recipe)
+        API.getRecipe(id).then(recipe => setRecipe(recipe))
     },[])
+
+    const addToBasket = () => {
+        setQtyCounter(qtyCounter + 1)
+        setOrder({quantity:qtyCounter,product_id:id})
+
+        const localOrders = [...orders]
+        const itemIndex = localOrders.findIndex((item) => item.id === id);
+
+        if (itemIndex !== -1) {
+        localOrders[itemIndex].quantity += qtyCounter;
+        } else {
+        localOrders.push({order});
+        }
+
+        
+        setOrders(localOrders)
+
+        console.log(orders)
+    }
 
     return  <div>
                 <h1>{recipe.title}</h1>
+                <button onClick={addToBasket}>ADD TO BASKET</button>
             </div>
 }
 
