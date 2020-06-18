456d42ab8502f7806af9520aee37985e2fa5a18f
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-18 02:57:50 +0100

    fetch each recipe

--- src/components/API.js
+++ src/components/API.js
@@ -1,7 +1,9 @@
 const baseURL = "http://localhost:3001"
     
 const getRecipes = () => fetch(`${baseURL}/Recipes`).then(recipe => recipe.json());
+const getRecipe = (id) => fetch(`${baseURL}/Recipes/${id}`).then(recipe => recipe.json());
  
 export default {
-    getRecipes
+    getRecipes,
+    getRecipe
 }
