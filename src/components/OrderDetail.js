1075db4479c29983822d1c1344967e76565f6bdb
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-18 23:57:53 +0100

    each order detail

---
+++ src/components/OrderDetail.js
@@ -0,0 +1,12 @@
+import React, { useState,useEffect } from 'react';
+import API from './API'
+import "./style/Recipes.scss"
+
+const OrderDetail = () => {
+
+    return  <div>
+                <h1>Quantity</h1>
+            </div>
+}
+
+export default OrderDetail;
