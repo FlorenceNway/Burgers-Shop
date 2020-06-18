f780b999b84b2e0537c20d2cc62d2a010cc0fb8a
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-18 02:57:41 +0100

    product detail id , routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import "./App.css";
 import Nav from "./components/Nav";
 import OnTheMenu from "./components/OnTheMenu";
 import Recipes from "./components/Recipes";
+import RecipeDetail from "./components/RecipeDetail";
 
 function App() {
 	
 	return 	<Router>
 				<div className="App">
 					<Nav/>
-					<Route path="/" exact component={OnTheMenu} />
-					<Route path="/recipes" exact component={Recipes} />
+					<Switch>
+						<Route exact path="/" component={OnTheMenu}/>
+						<Route exact path="/recipes" component={Recipes} />
+						<Route path="/recipes/:id" component={RecipeDetail} />
+					</Switch>
+					
 				</div>
 			</Router>
 }
