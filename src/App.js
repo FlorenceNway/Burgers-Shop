118258e5a2999e6d397545b3b6ee03f2f7568df0
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-18 23:57:17 +0100

    orderDetails

--- src/App.js
+++ src/App.js
@@ -5,16 +5,30 @@ import Nav from "./components/Nav";
 import OnTheMenu from "./components/OnTheMenu";
 import Recipes from "./components/Recipes";
 import RecipeDetail from "./components/RecipeDetail";
+import OrderDetail from "./components/OrderDetail";
 
 function App() {
-	
+	const [qtyCounter, setQtyCounter] = useState(0)
+	const [orders, setOrders] = useState([])
+
 	return 	<Router>
 				<div className="App">
-					<Nav/>
+					<Nav qtyCounter={qtyCounter}/>
 					<Switch>
 						<Route exact path="/" component={OnTheMenu}/>
 						<Route exact path="/recipes" component={Recipes} />
-						<Route path="/recipes/:id" component={RecipeDetail} />
+
+						<Route
+							path='/recipes/:id'
+							render={(props) => (
+								<RecipeDetail {...props} setQtyCounter={setQtyCounter} qtyCounter={qtyCounter} 
+								/>
+							)}
+						/>
+
+						<Route path="/cart" >
+							<OrderDetail qtyCounter={qtyCounter}/>
+						</Route>
 					</Switch>
 					
 				</div>
