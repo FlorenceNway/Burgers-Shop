69d92fd2c19d95b63bbaf161a670f403267445aa
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-18 23:57:25 +0100

    post and patch

--- src/components/API.js
+++ src/components/API.js
@@ -1,9 +1,30 @@
 const baseURL = "http://localhost:3001"
     
 const getRecipes = () => fetch(`${baseURL}/Recipes`).then(recipe => recipe.json());
+
 const getRecipe = (id) => fetch(`${baseURL}/Recipes/${id}`).then(recipe => recipe.json());
+
+const postCustomer = (customer) =>  fetch(baseURL, {
+                                            method: "POST",
+                                            headers: {
+                                            Accept: "application/json",
+                                            "Content-Type": "application/json",
+                                            },
+                                        body: JSON.stringify(customer),
+                                    }).then((res) => res.json());
+
+const patchOrder = (id,order) => fetch(`${baseURL}/${id}`, {
+                                            method: "PATCH",
+                                            headers: {
+                                            Accept: "application/json",
+                                            "Content-Type": "application/json",
+                                            },
+                                        body: JSON.stringify(order),
+                                    }).then((res) => res.json());
  
 export default {
     getRecipes,
-    getRecipe
+    getRecipe,
+    postCustomer,
+    patchOrder
 }
