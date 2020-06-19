eea609e0ce34cbe86f2c7a60fbfb2d2b2a531739
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-19 03:21:38 +0100

    text and background added

--- src/components/OnTheMenu.js
+++ src/components/OnTheMenu.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
+import './style/OnTheMenu.scss'
 
 const OnTheMenu = () => {
-    return <div>
-        <h1>ON THE MENU</h1>
-        <NavLink to="/recipes" activeClassName={'active'}>
-            <button>GET STARTED</button>
-        </NavLink>
-    </div>
+    return <div className="coverPage">
+                <div className="coverText">
+                    <h1> Order our top-rated recipes today!</h1>
+                    <NavLink to="/recipes" activeClassName={'active'}>
+                        <button>GET STARTED</button>
+                    </NavLink>
+                </div>
+            </div>
 }
 
 export default OnTheMenu;
