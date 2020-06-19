d912db1d3ba6979f7b79f777fd96bc0c11ae8b4f
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-19 16:54:47 +0100

    addTobasket to work

--- src/components/RecipeDetail.js
+++ src/components/RecipeDetail.js
@@ -1,44 +1,34 @@
 import React, { useState,useEffect } from 'react';
 import API from './API'
-import "./style/Recipes.scss"
+import "./style/Recipe.scss"
 
-const RecipeDetail = ({match, setQtyCounter, qtyCounter}) => {
+const RecipeDetail = ({match, setQtyCounter, qtyCounter, order, setOrder}) => {
     
     const [recipe, setRecipe] = useState({})
-    const [orders, setOrders] = useState([])
-    const [order, setOrder] = useState({
-            "quantity": 0,
-            "product_id": null
-    })
-    
     const id = match.params.id
 
     useEffect(() => {
         API.getRecipe(id).then(recipe => setRecipe(recipe))
     },[])
 
+
     const addToBasket = () => {
+
         setQtyCounter(qtyCounter + 1)
-        setOrder({quantity:qtyCounter,product_id:id})
 
-        const localOrders = [...orders]
-        const itemIndex = localOrders.findIndex((item) => item.id === id);
-
-        if (itemIndex !== -1) {
-        localOrders[itemIndex].quantity += qtyCounter;
-        } else {
-        localOrders.push({order});
-        }
-
-        
-        setOrders(localOrders)
-
-        console.log(orders)
+        setOrder({...order, id: id, qty: 1, recipe: recipe})
     }
-
-    return  <div>
-                <h1>{recipe.title}</h1>
-                <button onClick={addToBasket}>ADD TO BASKET</button>
+    
+    return  <div className="recipeDetail">
+            <div className={'recipe'}>
+                <div className='detail'>
+                    <h1>{recipe.title}</h1>
+                     <button onClick={addToBasket}>ADD TO BASKET</button>
+                </div>
+                <div className='photo'>
+                    <img src={`../burgers/${recipe.photo}`}></img>
+                </div>
+            </div>
             </div>
 }
 
