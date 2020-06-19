1d1c80ddf1fe4f8a30a660508d1b079a2ee7e85f
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-19 16:56:11 +0100

    recipes rendering

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,5 +1,7 @@
 import React, { useState,useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import clock from "../images/clock.svg"
+import Cover from './Cover';
 import API from './API'
 import "./style/Recipes.scss"
 
@@ -11,18 +13,25 @@ const Recipes = () => {
         API.getRecipes().then(recipes => setRecipes(recipes))
     },[])
 
-    return  <div>
-                <h1>Recipes</h1>
-                <button>Get Started</button>
+    return  <div className="recipesPage">
+                <Cover />
+                <section className="text">
+                    <div>
+                        <h1>Explore our Menus</h1>
+                        <p>Choose from an ever-changing mix of meat, fish, Beyond Meat™, WW Approved, Diabetes Friendly recipes and health-conscious offerings.</p>
+                    </div> 
+                </section>
+                
                 <ul className="recipes">
                 {recipes.map(recipe => (
                     <Link to={`/recipes/${recipe.id}`}>
                         <div className="recipe">
-                            <li key={recipe.title}>{recipe.title}</li>
-                            <li key={recipe.id}>{recipe.description}</li>
-                            <li key={recipe.photo}>
+                             <li key={recipe.photo} className="recipePhoto">
                                 <img src={`./burgers/${recipe.photo}`}></img>
                             </li>
+                            <li key={recipe.title}>{recipe.title}</li>
+                            <li key={recipe.id}>{recipe.description}</li>
+                            <li><img src={clock}></img><span>{recipe.time}</span></li>
                         </div>
                     </Link>
                     
