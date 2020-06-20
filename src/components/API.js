28b51274fe7557cc5f7d3076afc6534db0ced02c
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-20 19:16:25 +0100

    postCustomers/ postOrder

--- src/components/API.js
+++ src/components/API.js
@@ -1,8 +1,9 @@
 const baseURL = "http://localhost:3001"
     
-const getRecipes = () => fetch(`${baseURL}/Recipes`).then(recipe => recipe.json());
+const getRecipes = (number) => fetch(`${baseURL}/Recipes?_page=1&_limit=${number}`).then(recipe => recipe.json());
 
 const getRecipe = (id) => fetch(`${baseURL}/Recipes/${id}`).then(recipe => recipe.json());
+const getCustomers = () => fetch(`${baseURL}/Customers`).then(customers => customers.json());
 
 const postCustomer = (customer) =>  
             fetch(`${baseURL}/customers`, 
@@ -25,7 +26,14 @@ const postOrder = (order) =>
                 "Content-Type": "application/json",
                 },
             body: JSON.stringify(order),
-        }).then((res) => res.json());
+        }).then((response) => {
+            if(response.ok) {
+                return response.json()
+            }else {
+                throw "Oops we couldn't post that!"
+            }
+        })
+        .catch(error =>  error );
 
 
 const patchOrder = (id,order) => 
@@ -42,6 +50,7 @@ const patchOrder = (id,order) =>
             
 export default {
     getRecipes,
+    getCustomers,
     getRecipe,
     postCustomer,
     postOrder,
