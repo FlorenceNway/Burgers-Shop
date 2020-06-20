158fceadf0b930f8df7f28780e5a80ae0b8b99ef
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-20 05:25:50 +0100

    recipe detail style

--- src/components/RecipeDetail.js
+++ src/components/RecipeDetail.js
@@ -23,14 +23,17 @@ const RecipeDetail = ({match, setQtyCounter, qtyCounter, order, setOrder}) => {
     return  <div className="recipeDetail">
                 <div className={'recipe'}>
                     <div className='detail'>
-                        <h3>{recipe.title}</h3>
-                        <p><img src={require('../images/clock.svg')}></img>{recipe.time}</p>
-                        <div>
-                            <p><img src={require('../images/clock.svg')}>{recipe.serving}</img></p>
-                            <p><img src={require('../images/clock.svg')}></img>820 cals/serving</p>
+                        <div className="detailText">
+
+                            <h3>{recipe.title}</h3>
+                            <p className={'time'}><img src={require('../images/clock.svg')}></img>{recipe.time}</p>
+                            <div>
+                                <p><img src={require('../images/utensils.png')}></img>{recipe.serve}</p>
+                                <p><img src={require('../images/calori.png')}></img>820 cals/serving</p>
+                            </div>
+                            <p>Plant-based burger, THIS Isn’t Bacon, vegan smoked gouda, shoestring fries, vegan bacon ketchup, chipotle ‘mayo’, spinach and pickles with rosemary salted chips</p>
+                            <button onClick={addToBasket}>ADD TO BASKET</button>
                         </div>
-                        <p>Plant-based burger, THIS Isn’t Bacon, vegan smoked gouda, shoestring fries, vegan bacon ketchup, chipotle ‘mayo’, spinach and pickles with rosemary salted chips</p>
-                        <button onClick={addToBasket}>ADD TO BASKET</button>
                     </div>
                     <div className='photo'>
                         <img src={`../burgers/${recipe.photo}`}></img>
