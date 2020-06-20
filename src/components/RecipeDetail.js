52207c2cf6610f440bec4faf4988b7ef1cd83e9a
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-20 02:56:04 +0100

    import separated components

--- src/components/RecipeDetail.js
+++ src/components/RecipeDetail.js
@@ -1,6 +1,8 @@
 import React, { useState,useEffect } from 'react';
-import API from './API'
-import "./style/Recipe.scss"
+import API from './API';
+import Ingredient from './Ingredient';
+import SpecialOffer from './SpecialOffer';
+import "./style/RecipeDetail.scss"
 
 const RecipeDetail = ({match, setQtyCounter, qtyCounter, order, setOrder}) => {
     
@@ -14,21 +16,28 @@ const RecipeDetail = ({match, setQtyCounter, qtyCounter, order, setOrder}) => {
 
     const addToBasket = () => {
 
-        setQtyCounter(qtyCounter + 1)
-
         setOrder({...order, id: id, qty: 1, recipe: recipe})
+        
     }
     
     return  <div className="recipeDetail">
-            <div className={'recipe'}>
-                <div className='detail'>
-                    <h1>{recipe.title}</h1>
-                     <button onClick={addToBasket}>ADD TO BASKET</button>
+                <div className={'recipe'}>
+                    <div className='detail'>
+                        <h3>{recipe.title}</h3>
+                        <p><img src={require('../images/clock.svg')}></img>{recipe.time}</p>
+                        <div>
+                            <p><img src={require('../images/clock.svg')}>{recipe.serving}</img></p>
+                            <p><img src={require('../images/clock.svg')}></img>820 cals/serving</p>
+                        </div>
+                        <p>Plant-based burger, THIS Isn’t Bacon, vegan smoked gouda, shoestring fries, vegan bacon ketchup, chipotle ‘mayo’, spinach and pickles with rosemary salted chips</p>
+                        <button onClick={addToBasket}>ADD TO BASKET</button>
+                    </div>
+                    <div className='photo'>
+                        <img src={`../burgers/${recipe.photo}`}></img>
+                    </div>
                 </div>
-                <div className='photo'>
-                    <img src={`../burgers/${recipe.photo}`}></img>
-                </div>
-            </div>
+                <Ingredient />
+                <SpecialOffer />
             </div>
 }
 
