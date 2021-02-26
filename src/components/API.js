4296b8ad39035a2563111ab89c4210755115f3f8
Florence Nway <FlorenceNway@users.noreply.github.com>
2021-02-26 22:25:15 +0000

    adjust alignment

--- src/components/API.js
+++ src/components/API.js
@@ -1,59 +1,58 @@
-//const baseURL = "http://localhost:3001"
-const baseURL = "https://tested-rose-silver.glitch.me";
-    
-const getRecipes = (number) => fetch(`${baseURL}/Recipes?_page=1&_limit=${number}`).then(recipe => recipe.json());
+const baseURL = "http://localhost:3001";
 
-const getRecipe = (id) => fetch(`${baseURL}/Recipes/${id}`).then(recipe => recipe.json());
-const getCustomers = () => fetch(`${baseURL}/Customers`).then(customers => customers.json());
+const getRecipes = (number) =>
+  fetch(`${baseURL}/Recipes?_page=1&_limit=${number}`).then((recipe) =>
+    recipe.json()
+  );
 
-const postCustomer = (customer) =>  
-            fetch(`${baseURL}/customers`, 
-            {
-                method: "POST",
-                headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-                },
-            body: JSON.stringify(customer),
-        }).then((res) => res.json());
+const getRecipe = (id) =>
+  fetch(`${baseURL}/Recipes/${id}`).then((recipe) => recipe.json());
+const getCustomers = () =>
+  fetch(`${baseURL}/Customers`).then((customers) => customers.json());
 
+const postCustomer = (customer) =>
+  fetch(`${baseURL}/customers`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(customer),
+  }).then((res) => res.json());
 
-const postOrder = (order) =>  
-            fetch(`${baseURL}/orders`, 
-            {
-                method: "POST",
-                headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-                },
-            body: JSON.stringify(order),
-        }).then((response) => {
-            if(response.ok) {
-                return response.json()
-            }else {
-                throw "Oops we couldn't post that!"
-            }
-        })
-        .catch(error =>  error );
+const postOrder = (order) =>
+  fetch(`${baseURL}/orders`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(order),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw "Oops we couldn't post that!";
+      }
+    })
+    .catch((error) => error);
 
+const patchOrder = (id, order) =>
+  fetch(`${baseURL}/${id}`, {
+    method: "PATCH",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(order),
+  }).then((res) => res.json());
 
-const patchOrder = (id,order) => 
-                fetch(`${baseURL}/${id}`, 
-                {
-                    method: "PATCH",
-                    headers: {
-                    Accept: "application/json",
-                    "Content-Type": "application/json",
-                    },
-                body: JSON.stringify(order),
-            }).then((res) => res.json());
- 
-            
 export default {
-    getRecipes,
-    getCustomers,
-    getRecipe,
-    postCustomer,
-    postOrder,
-    patchOrder
-}
+  getRecipes,
+  getCustomers,
+  getRecipe,
+  postCustomer,
+  postOrder,
+  patchOrder,
+};
